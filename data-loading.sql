a8798fa15ce51a1e99a742d214be37e2ae893c09
rocky-mtn-high <rhcastro@mines.edu>
2023-11-27 12:07:47 -0700

    Parks and rec data

--- data-loading.sql
+++ data-loading.sql
@@ -1,5 +1,6 @@
 SET search_path TO f23_group2;
 
+--The office first
 --Episodes
 DROP TABLE IF EXISTS office_episodes CASCADE;
 CREATE TABLE office_episodes (
@@ -14,7 +15,7 @@ CREATE TABLE office_episodes (
     us_viewers FLOAT
 );
 ALTER TABLE office_episodes ADD PRIMARY KEY(prod_code); 
-
+ALTER TABLE f23_group2.office_episodes OWNER TO f23_group2;
 \COPY office_episodes FROM './CSCI403/final_project/data/office_episodes.csv' WITH CSV HEADER;
 
 --IMDB ratings
@@ -29,5 +30,42 @@ CREATE TABLE office_ratings (
    description TEXT
 );
 ALTER TABLE office_ratings ADD PRIMARY KEY(season, episode_in_season); 
+ALTER TABLE f23_group2.office_ratings OWNER TO f23_group2;
+
 
 \COPY office_ratings FROM './CSCI403/final_project/data/office_imdb.csv' WITH CSV HEADER;
+
+
+
+--Then parks and rec
+DROP TABLE IF EXISTS parks_and_rec_episodes CASCADE;
+CREATE TABLE parks_and_rec_episodes (
+    season INT,
+    episode_num_in_season INT,
+    episode_num_overall INT,
+    title TEXT,
+    directed_by TEXT,
+    writen_by TEXT,
+    original_air_date DATE,
+    us_viewers FLOAT
+);
+ALTER TABLE parks_and_rec_episodes ADD PRIMARY KEY(episode_num_overall); 
+ALTER TABLE f23_group2.parks_and_rec_episodes OWNER TO f23_group2;
+\COPY parks_and_rec_episodes FROM './CSCI403/final_project/data/parks_and_rec_episodes.csv' WITH CSV HEADER;
+
+--IMDB ratings
+DROP TABLE IF EXISTS parks_and_rec_ratings CASCADE;
+CREATE TABLE parks_and_rec_ratings (
+   season INT,
+   episode_in_season INT, 
+   title TEXT,
+   air_date DATE,
+   rating FLOAT,
+   total_votes INT,
+   description TEXT
+);
+ALTER TABLE parks_and_rec_ratings ADD PRIMARY KEY(season, episode_in_season); 
+ALTER TABLE f23_group2.parks_and_rec_ratings OWNER TO f23_group2;
+
+
+\COPY office_ratings FROM './CSCI403/final_project/data/parks_and_rec_imdb.csv' WITH CSV HEADER;
