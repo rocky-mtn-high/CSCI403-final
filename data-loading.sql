09ecdba7bcdd65ff9f081f3ba1869b2d5f1d2c1a
chuhanuman <chuhanuman@yahoo.com>
2023-11-27 12:19:43 -0700

    Fixed errors

--- data-loading.sql
+++ data-loading.sql
@@ -16,7 +16,7 @@ CREATE TABLE office_episodes (
 );
 ALTER TABLE office_episodes ADD PRIMARY KEY(prod_code); 
 ALTER TABLE f23_group2.office_episodes OWNER TO f23_group2;
-\COPY office_episodes FROM './CSCI403/final_project/data/office_episodes.csv' WITH CSV HEADER;
+\COPY office_episodes FROM 'data/office_episodes.csv' WITH CSV HEADER;
 
 --IMDB ratings
 DROP TABLE IF EXISTS office_ratings CASCADE;
@@ -33,7 +33,7 @@ ALTER TABLE office_ratings ADD PRIMARY KEY(season, episode_in_season);
 ALTER TABLE f23_group2.office_ratings OWNER TO f23_group2;
 
 
-\COPY office_ratings FROM './CSCI403/final_project/data/office_imdb.csv' WITH CSV HEADER;
+\COPY office_ratings FROM 'data/office_imdb.csv' WITH CSV HEADER;
 
 
 
@@ -51,7 +51,7 @@ CREATE TABLE parks_and_rec_episodes (
 );
 ALTER TABLE parks_and_rec_episodes ADD PRIMARY KEY(episode_num_overall); 
 ALTER TABLE f23_group2.parks_and_rec_episodes OWNER TO f23_group2;
-\COPY parks_and_rec_episodes FROM './CSCI403/final_project/data/parks_and_rec_episodes.csv' WITH CSV HEADER;
+\COPY parks_and_rec_episodes FROM 'data/parks_and_rec_episodes.csv' WITH CSV HEADER;
 
 --IMDB ratings
 DROP TABLE IF EXISTS parks_and_rec_ratings CASCADE;
@@ -68,4 +68,4 @@ ALTER TABLE parks_and_rec_ratings ADD PRIMARY KEY(season, episode_in_season);
 ALTER TABLE f23_group2.parks_and_rec_ratings OWNER TO f23_group2;
 
 
-\COPY office_ratings FROM './CSCI403/final_project/data/parks_and_rec_imdb.csv' WITH CSV HEADER;
+\COPY parks_and_rec_ratings FROM 'data/parks_and_rec_imdb.csv' WITH CSV HEADER;
